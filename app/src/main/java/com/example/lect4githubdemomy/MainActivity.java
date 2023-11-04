1d76a1a2b87a9b831b2c01c1effff4a85a2245a6
Taylor <56862331+Taylor0813@users.noreply.github.com>
2023-11-04 13:59:21 -0700

    Update MainActivity.java

--- app/src/main/java/com/example/lect4githubdemomy/MainActivity.java
+++ app/src/main/java/com/example/lect4githubdemomy/MainActivity.java
@@ -8,6 +8,8 @@ public class MainActivity extends AppCompatActivity {
 
     //made some changes locally
 
+    //Another update
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
