0d87b059c4e3f1363beb5a72ac0f4815e1f6aab4
yifeizhang <yifei.academic1@gmail.com>
2023-11-04 13:57:39 -0700

    First commit to add a comment line

--- app/src/main/java/com/example/lect4githubdemomy/MainActivity.java
+++ app/src/main/java/com/example/lect4githubdemomy/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //made some changes locally
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
